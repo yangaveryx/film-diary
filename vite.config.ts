e819a89ce0d2a5565d3e0edb6d5886e95bbb80fe
yangaveryx <yang.averyx@gmail.com>
2026-05-04 05:44:24 -0400

    Updated config

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   root: 'src',
+  envDir: '..',
   build: {
     outDir: '../dist-client',
     emptyOutDir: true,
-  }
+  },
 })
